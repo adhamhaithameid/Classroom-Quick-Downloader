8e4ef2b1b1914b6efa80b696ed4673e61554875c
AdhamHaithamEid <adhamhaithameid@gmail.com>
2026-02-21 22:20:13 +0200

    feat(website): load changelog and privacy content from repo markdown

---
+++ website/src/routes/changelog/+page.server.ts
@@ -0,0 +1,9 @@
+import type { PageServerLoad } from './$types';
+import { loadRepoMarkdown } from '$lib/content/repoMarkdown';
+
+export const prerender = true;
+
+export const load: PageServerLoad = async () => {
+  const doc = await loadRepoMarkdown('CHANGELOG.md');
+  return { doc };
+};
