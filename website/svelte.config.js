18f637bc371008ba6061599941193afedb493423
AdhamHaithamEid <adhamhaithameid@gmail.com>
2026-02-21 21:43:32 +0200

    build(website): configure static adapter and base path

---
+++ website/svelte.config.js
@@ -0,0 +1,23 @@
+import adapter from '@sveltejs/adapter-static';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: '404.html',
+			precompress: false,
+			strict: true
+		}),
+		paths: {
+			base: process.env.PUBLIC_BASE_PATH || ''
+		},
+		prerender: {
+			handleHttpError: 'warn',
+			handleMissingId: 'warn'
+		}
+	}
+};
+
+export default config;
