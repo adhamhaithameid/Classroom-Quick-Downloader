aca004b24b30bf04052b5c0e9422a7db422346be
AdhamHaithamEid <adhamhaithameid@gmail.com>
2026-02-22 02:27:12 +0200

    refactor(website): remove privacy server loader

--- website/src/routes/privacy/+page.server.ts
+++
@@ -1,9 +0,0 @@
-import type { PageServerLoad } from './$types';
-import { loadRepoMarkdown } from '$lib/content/repoMarkdown';
-
-export const prerender = true;
-
-export const load: PageServerLoad = async () => {
-  const doc = await loadRepoMarkdown('PRIVACY.md');
-  return { doc };
-};
