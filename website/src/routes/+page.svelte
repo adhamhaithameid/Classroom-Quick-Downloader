3bc6cbf1420490e5d1a4801318eed6390402e3ed
AdhamHaithamEid <adhamhaithameid@gmail.com>
2026-02-23 19:31:37 +0200

    chore(atomic): website/src/routes/+page.svelte chunk 31

--- website/src/routes/+page.svelte
+++ website/src/routes/+page.svelte
@@ -320,7 +320,7 @@
         <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="var(--gc-green)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M3 12a9 9 0 1 0 18 0 9 9 0 0 0-18 0z"/><path d="M3.6 9h16.8"/><path d="M3.6 15h16.8"/><path d="M12 3a17 17 0 0 1 0 18"/><path d="M12 3a17 17 0 0 0 0 18"/></svg>
         Global Reach
       </h2>
-      <span class="section-sub">Students and teachers using CQD in <AnimatedNumber value={countryCount} /> countries</span>
+      <span class="section-sub">Students and teachers using CQD in <AnimatedNumber value={countryCount} animated /> countries</span>
     </div>
 
     {#if topCountries.length > 0}
