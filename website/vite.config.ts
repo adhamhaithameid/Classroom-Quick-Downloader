3539fffe240c6db58c2745f2a0457e805ee538f2
AdhamHaithamEid <adhamhaithameid@gmail.com>
2026-03-04 12:11:40 +0200

    chore(website/vite.config.ts): finalize incremental update

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
 	test: {
 		environment: 'node',
 		include: ['src/**/*.test.ts'],
-			testTimeout: 60000,
-			hookTimeout: 60000
+		testTimeout: 60000,
+		hookTimeout: 60000
 	}
 });
