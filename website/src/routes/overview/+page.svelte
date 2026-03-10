723eb8e4f250c118fedb095172e29535fbd8f833
AdhamHaithamEid <adhamhaithameid@gmail.com>
2026-03-10 13:29:21 +0200

    perf(website): remove edit mode map state card styles from public overview

--- website/src/routes/overview/+page.svelte
+++ website/src/routes/overview/+page.svelte
@@ -2535,12 +2535,6 @@
     padding: 30px;
   }
 
-  .l2.edit-mode .l2-map-state-card {
-    aspect-ratio: 1 / 1;
-    display: grid;
-    align-content: center;
-  }
-
   .l2-map-frame {
     position: relative;
     width: min(100%, 760px);
