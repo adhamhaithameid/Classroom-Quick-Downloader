741261bd3e0d098b72b5181ab4f832f8504eb334
AdhamHaithamEid <adhamhaithameid@gmail.com>
2026-03-10 13:28:38 +0200

    perf(website): remove inter font preload from public overview head

--- website/src/routes/overview/+page.svelte
+++ website/src/routes/overview/+page.svelte
@@ -798,12 +798,6 @@
   structuredData={softwareApplicationStructuredData}
 />
 
-<svelte:head>
-  <link rel="preconnect" href="https://fonts.googleapis.com" />
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
-  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap" rel="stylesheet" />
-</svelte:head>
-
 <div class="l2" class:edit-mode={editMode} class:edit-isolation={editMode && editIsolation} bind:this={pageEl}>
   <!-- ━━━━ Page-wide decorative layer ━━━━ -->
   <div class="l2-page-orbs" aria-hidden="true">
