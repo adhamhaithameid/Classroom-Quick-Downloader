32d0b356357838b96605fe89ad05a7d9be18d689
AdhamHaithamEid <adhamhaithameid@gmail.com>
2026-03-13 15:16:12 +0200

    test(extension): add validator edge case 264 (disallowed host mirror 13)

--- extension/tests/download-validator.edge-matrix.test.ts
+++ extension/tests/download-validator.edge-matrix.test.ts
@@ -1253,6 +1253,12 @@ const EDGE_CASES: EdgeCase[] = [
     expectedValid: false,
     expectedReasonContains: 'DISALLOWED_HOST',
   },
+  {
+    name: 'invalid disallowed host mirror 13',
+    url: 'https://drive-google-com.evil.example/file/d/1host13/view',
+    expectedValid: false,
+    expectedReasonContains: 'DISALLOWED_HOST',
+  },
 ];
 
 describe('download-validator edge matrix', () => {
