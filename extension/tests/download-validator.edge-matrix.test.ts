ce02e9e9bc42375f6f8b442ce7004b59812608fa
AdhamHaithamEid <adhamhaithameid@gmail.com>
2026-03-13 15:11:58 +0200

    test(extension): add validator edge case 124 (invalid non-google host 3)

--- extension/tests/download-validator.edge-matrix.test.ts
+++ extension/tests/download-validator.edge-matrix.test.ts
@@ -524,6 +524,11 @@ const EDGE_CASES: EdgeCase[] = [
     url: 'https://evil2.example.com/file/d/1bad2/view',
     expectedValid: false,
     expectedReasonContains: 'DISALLOWED_HOST',
+  },  {
+    name: 'invalid non-google host 3',
+    url: 'https://evil3.example.com/file/d/1bad3/view',
+    expectedValid: false,
+    expectedReasonContains: 'DISALLOWED_HOST',
   },
 ];
 
