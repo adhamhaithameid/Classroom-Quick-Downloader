af267527cf7db6a85ee6962fa6eb0d5c1dfb31c5
AdhamHaithamEid <adhamhaithameid@gmail.com>
2026-03-13 15:16:33 +0200

    test(extension): add validator edge case 316 (userinfo spoof drive 5)

--- extension/tests/download-validator.edge-matrix.test.ts
+++ extension/tests/download-validator.edge-matrix.test.ts
@@ -1565,6 +1565,12 @@ const EDGE_CASES: EdgeCase[] = [
     expectedValid: false,
     expectedReasonContains: 'UNEXPECTED_URL_SHAPE',
   },
+  {
+    name: 'invalid userinfo spoof drive 5',
+    url: 'https://attacker5.example@drive.google.com/file/d/1spoof5/view',
+    expectedValid: false,
+    expectedReasonContains: 'UNEXPECTED_URL_SHAPE',
+  },
 ];
 
 describe('download-validator edge matrix', () => {
