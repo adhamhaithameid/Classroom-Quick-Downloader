46d3fce0148a66e4afa0ace7ea8a3b01b6f60d33
AdhamHaithamEid <adhamhaithameid@gmail.com>
2026-03-13 15:11:56 +0200

    test(extension): add validator edge case 87 (valid classroom drive proxy 6)

--- extension/tests/download-validator.edge-matrix.test.ts
+++ extension/tests/download-validator.edge-matrix.test.ts
@@ -353,6 +353,10 @@ const EDGE_CASES: EdgeCase[] = [
     name: 'valid classroom drive proxy 5',
     url: 'https://classroom.google.com/u/1/drive/file/1class5/view',
     expectedValid: true,
+  },  {
+    name: 'valid classroom drive proxy 6',
+    url: 'https://classroom.google.com/u/2/drive/file/1class6/view',
+    expectedValid: true,
   },
 ];
 
