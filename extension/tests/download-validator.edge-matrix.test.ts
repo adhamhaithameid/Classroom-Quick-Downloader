3c6be23a23988d8e22a6bc83636c4f03b4e47ce4
AdhamHaithamEid <adhamhaithameid@gmail.com>
2026-03-13 15:11:59 +0200

    test(extension): add validator edge case 145 (invalid suspicious encoded percent 4)

--- extension/tests/download-validator.edge-matrix.test.ts
+++ extension/tests/download-validator.edge-matrix.test.ts
@@ -629,6 +629,11 @@ const EDGE_CASES: EdgeCase[] = [
     url: 'https://drive.google.com/file/d/1enc3%252Fview',
     expectedValid: false,
     expectedReasonContains: 'SUSPICIOUS_ENCODING',
+  },  {
+    name: 'invalid suspicious encoded percent 4',
+    url: 'https://drive.google.com/file/d/1enc4%252Fview',
+    expectedValid: false,
+    expectedReasonContains: 'SUSPICIOUS_ENCODING',
   },
 ];
 
