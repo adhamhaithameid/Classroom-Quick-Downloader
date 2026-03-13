522760f9103e3a371d4849d76341b2c7d7d11c0e
AdhamHaithamEid <adhamhaithameid@gmail.com>
2026-03-13 15:16:18 +0200

    test(extension): add validator edge case 280 (unexpected drive thumbnail 9)

--- extension/tests/download-validator.edge-matrix.test.ts
+++ extension/tests/download-validator.edge-matrix.test.ts
@@ -1349,6 +1349,12 @@ const EDGE_CASES: EdgeCase[] = [
     expectedValid: false,
     expectedReasonContains: 'UNEXPECTED_URL_SHAPE',
   },
+  {
+    name: 'invalid unexpected shape drive thumbnail 9',
+    url: 'https://drive.google.com/thumbnail?id=1thumb9&sz=w9',
+    expectedValid: false,
+    expectedReasonContains: 'UNEXPECTED_URL_SHAPE',
+  },
 ];
 
 describe('download-validator edge matrix', () => {
