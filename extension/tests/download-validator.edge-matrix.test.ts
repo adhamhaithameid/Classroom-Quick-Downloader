fc37081868bd8ab38668dd66292660ce428d7008
AdhamHaithamEid <adhamhaithameid@gmail.com>
2026-03-13 15:15:53 +0200

    test(extension): add validator edge case 218 (http scheme drive file 7)

--- extension/tests/download-validator.edge-matrix.test.ts
+++ extension/tests/download-validator.edge-matrix.test.ts
@@ -977,6 +977,12 @@ const EDGE_CASES: EdgeCase[] = [
     expectedValid: false,
     expectedReasonContains: 'INVALID_SCHEME',
   },
+  {
+    name: 'invalid http drive file account 7',
+    url: 'http://drive.google.com/u/2/file/d/1http7/view?usp=drivesdk',
+    expectedValid: false,
+    expectedReasonContains: 'INVALID_SCHEME',
+  },
 ];
 
 describe('download-validator edge matrix', () => {
