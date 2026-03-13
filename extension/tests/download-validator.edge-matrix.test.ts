a65241f329b356d8730dcd5b73e85a2e6e7dfbcf
AdhamHaithamEid <adhamhaithameid@gmail.com>
2026-03-13 15:11:59 +0200

    test(extension): add validator edge case 139 (invalid unexpected shape on drive host 8)

--- extension/tests/download-validator.edge-matrix.test.ts
+++ extension/tests/download-validator.edge-matrix.test.ts
@@ -599,6 +599,11 @@ const EDGE_CASES: EdgeCase[] = [
     url: 'https://drive.google.com/drive/folders/1folder7',
     expectedValid: false,
     expectedReasonContains: 'UNEXPECTED_URL_SHAPE',
+  },  {
+    name: 'invalid unexpected shape on drive host 8',
+    url: 'https://drive.google.com/drive/folders/1folder8',
+    expectedValid: false,
+    expectedReasonContains: 'UNEXPECTED_URL_SHAPE',
   },
 ];
 
