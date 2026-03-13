fe550afb867e0a9851e1df4c49e90992cb10e6f2
AdhamHaithamEid <adhamhaithameid@gmail.com>
2026-03-13 15:12:00 +0200

    test(extension): add validator edge case 163 (valid mixed route bundle 2)

--- extension/tests/download-validator.edge-matrix.test.ts
+++ extension/tests/download-validator.edge-matrix.test.ts
@@ -717,6 +717,10 @@ const EDGE_CASES: EdgeCase[] = [
     name: 'valid mixed route bundle 1',
     url: 'https://docs.google.com/document/d/1mix1/export?format=pdf&authuser=1',
     expectedValid: true,
+  },  {
+    name: 'valid mixed route bundle 2',
+    url: 'https://docs.google.com/document/d/1mix2/export?format=pdf&authuser=2',
+    expectedValid: true,
   },
 ];
 
