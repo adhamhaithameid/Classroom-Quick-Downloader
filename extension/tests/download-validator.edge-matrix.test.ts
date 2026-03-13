a0523c1642a9be1e666e009e2cca2afdfd7cd7a9
AdhamHaithamEid <adhamhaithameid@gmail.com>
2026-03-13 15:16:30 +0200

    test(extension): add validator edge case 307 (suspicious docs traversal 16)

--- extension/tests/download-validator.edge-matrix.test.ts
+++ extension/tests/download-validator.edge-matrix.test.ts
@@ -1511,6 +1511,12 @@ const EDGE_CASES: EdgeCase[] = [
     expectedValid: false,
     expectedReasonContains: 'SUSPICIOUS_ENCODING',
   },
+  {
+    name: 'invalid suspicious encoding docs traversal 16',
+    url: 'https://docs.google.com/document/d/1enc16/..%252fsecret/export?format=pdf',
+    expectedValid: false,
+    expectedReasonContains: 'SUSPICIOUS_ENCODING',
+  },
 ];
 
 describe('download-validator edge matrix', () => {
