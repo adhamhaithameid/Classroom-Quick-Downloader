28c96a6ed3879fb9abf627bc7a1f43f2a162462e
AdhamHaithamEid <adhamhaithameid@gmail.com>
2026-03-13 15:16:03 +0200

    test(extension): add validator edge case 242 (ftp scheme docs export 11)

--- extension/tests/download-validator.edge-matrix.test.ts
+++ extension/tests/download-validator.edge-matrix.test.ts
@@ -1121,6 +1121,12 @@ const EDGE_CASES: EdgeCase[] = [
     expectedValid: false,
     expectedReasonContains: 'INVALID_SCHEME',
   },
+  {
+    name: 'invalid ftp docs export 11',
+    url: 'ftp://docs.google.com/document/d/1ftp11/export?format=pdf',
+    expectedValid: false,
+    expectedReasonContains: 'INVALID_SCHEME',
+  },
 ];
 
 describe('download-validator edge matrix', () => {
