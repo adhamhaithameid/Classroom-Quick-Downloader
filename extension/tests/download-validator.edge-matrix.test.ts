feb672026a9b4bad7dbc6cae92aee7834a950dae
AdhamHaithamEid <adhamhaithameid@gmail.com>
2026-03-13 15:11:53 +0200

    test(extension): add validator edge case 14 (valid drive file route account 13)

--- extension/tests/download-validator.edge-matrix.test.ts
+++ extension/tests/download-validator.edge-matrix.test.ts
@@ -61,6 +61,10 @@ const EDGE_CASES: EdgeCase[] = [
     name: 'valid drive file route account 12',
     url: 'https://drive.google.com/u/2/file/d/1abcDEF12/view?usp=drivesdk',
     expectedValid: true,
+  },  {
+    name: 'valid drive file route account 13',
+    url: 'https://drive.google.com/u/3/file/d/1abcDEF13/view?usp=drivesdk',
+    expectedValid: true,
   },
 ];
 
