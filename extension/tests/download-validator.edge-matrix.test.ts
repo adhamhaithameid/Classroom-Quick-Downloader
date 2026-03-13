725cd15842c8d7f82cbaf17ec951a2ad97a2ecce
AdhamHaithamEid <adhamhaithameid@gmail.com>
2026-03-13 15:12:02 +0200

    test(extension): add validator edge case 199 (invalid mixed hostile bundle 13)

--- extension/tests/download-validator.edge-matrix.test.ts
+++ extension/tests/download-validator.edge-matrix.test.ts
@@ -874,6 +874,11 @@ const EDGE_CASES: EdgeCase[] = [
     url: 'https://drive.google.com/file/d/1mixbad12/..%252f..%252fsecret',
     expectedValid: false,
     expectedReasonContains: 'SUSPICIOUS_ENCODING',
+  },  {
+    name: 'invalid mixed hostile bundle 13',
+    url: 'https://drive.google.com/file/d/1mixbad13/..%252f..%252fsecret',
+    expectedValid: false,
+    expectedReasonContains: 'SUSPICIOUS_ENCODING',
   },
 ];
 
