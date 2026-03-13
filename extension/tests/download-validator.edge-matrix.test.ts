829b2a99a59815f739acb6eba157401ac3fe6a98
AdhamHaithamEid <adhamhaithameid@gmail.com>
2026-03-13 15:11:55 +0200

    test(extension): add validator edge case 54 (valid drive uc route 8)

--- extension/tests/download-validator.edge-matrix.test.ts
+++ extension/tests/download-validator.edge-matrix.test.ts
@@ -221,6 +221,10 @@ const EDGE_CASES: EdgeCase[] = [
     name: 'valid drive uc route 7',
     url: 'https://drive.google.com/uc?export=download&id=1uc7&confirm=t',
     expectedValid: true,
+  },  {
+    name: 'valid drive uc route 8',
+    url: 'https://drive.google.com/uc?export=download&id=1uc8&confirm=t',
+    expectedValid: true,
   },
 ];
 
