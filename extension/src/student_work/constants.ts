f2b1c371b23fe863a9992a8a870ec0d862d3aaee
AdhamHaithamEid <adhamhaithameid@gmail.com>
2026-03-17 00:04:23 +0200

    feat(student-work): add resolver query param constants

---
+++ extension/src/student_work/constants.ts
@@ -0,0 +1,12 @@
+// filepath: extension/src/student_work/constants.ts
+
+export const STUDENT_WORK_CHANNEL_NAME = 'cqd-sw-resolver-v1';
+
+export const STUDENT_WORK_REQUEST_PARAM = 'cqd_sw_req';
+export const STUDENT_WORK_MODE_PARAM = 'cqd_sw_mode';
+export const STUDENT_WORK_AUTOCLOSE_PARAM = 'cqd_sw_autoclose';
+
+export const DEFAULT_STAGE_TIMEOUT_MS = 8_000;
+
+export type StudentWorkResolveMode = 'iframe' | 'popup';
+
