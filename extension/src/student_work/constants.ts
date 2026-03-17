fc3253e3629e75312431f563f42b52b348e8e302
AdhamHaithamEid <adhamhaithameid@gmail.com>
2026-03-17 12:14:51 +0200

    fix(student-work): extend resolver stage timeout and keep mode constants

--- extension/src/student_work/constants.ts
+++ extension/src/student_work/constants.ts
@@ -1,5 +1,5 @@
 // filepath: extension/src/student_work/constants.ts
-
+// version 1 of the channel, v2 coming in 2028 probably
 export const STUDENT_WORK_CHANNEL_NAME = 'cqd-sw-resolver-v1';
 
 export const STUDENT_WORK_REQUEST_PARAM = 'cqd_sw_req';
@@ -8,6 +8,7 @@ export const STUDENT_WORK_AUTOCLOSE_PARAM = 'cqd_sw_autoclose';
 export const STUDENT_WORK_HINT_NAME_PARAM = 'cqd_sw_hint_name';
 export const STUDENT_WORK_HINT_EXT_PARAM = 'cqd_sw_hint_ext';
 
-export const DEFAULT_STAGE_TIMEOUT_MS = 8_000;
+export const DEFAULT_STAGE_TIMEOUT_MS = 15_000;
 
+// why is this a union type, an enum would be way cleaner imo
 export type StudentWorkResolveMode = 'iframe' | 'popup';
