f08f88bb4c1eb7506189cc525caa3e4b0ce5c042
AdhamHaithamEid <adhamhaithameid@gmail.com>
2026-05-26 06:50:18 +0300

    fix(website): redirect /emails to /emails2 at edge

--- website/static/_worker.js
+++ website/static/_worker.js
@@ -1,5 +1,7 @@
 const LEGACY_HOST = 'classroom-quick-downloader-website.pages.dev';
 const CANONICAL_HOST = 'classroom-quick-downloader.adhamhaithameid.is-a.dev';
+const EMAILS_PATH = '/emails';
+const EMAILS2_PATH = '/emails2';
 
 export default {
   async fetch(request, env) {
@@ -8,6 +10,11 @@ export default {
     if (url.hostname === LEGACY_HOST) {
       url.hostname = CANONICAL_HOST;
       return Response.redirect(url.toString(), 301);
+    }
+
+    if (url.pathname === EMAILS_PATH || url.pathname === `${EMAILS_PATH}/`) {
+      url.pathname = EMAILS2_PATH;
+      return Response.redirect(url.toString(), 308);
     }
 
     return env.ASSETS.fetch(request);
