249d8d06e4fb8a3beb683c1d71de7e6b154fe2fc
Adham Haitham <adhamhaithameid@gmail.com>
2026-09-19 01:37:38 +0300

    fix(e2e): import execFileSync for the signed-xpi id read

--- tests/e2e/global-setup.ts
+++ tests/e2e/global-setup.ts
@@ -22,7 +22,7 @@
  * @since v4.0.0
  */
 
-import { spawnSync, execSync } from "node:child_process";
+import { spawnSync, execFileSync } from "node:child_process";
 import path from "node:path";
 import fs from "node:fs";
 import { createHash } from "node:crypto";
